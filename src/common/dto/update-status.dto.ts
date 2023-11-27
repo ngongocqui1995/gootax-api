9df33deb0d48dde40579ce3d691038a5e47b1e53
Qui <qui.ngo@finviet.com.vn>
2023-11-27 23:01:47 +0700

    update

--- src/common/dto/update-status.dto.ts
+++ src/common/dto/update-status.dto.ts
@@ -1,20 +1,14 @@
-import {
-    IsIn,
-    IsNotEmpty,
-    IsString,
-  } from 'class-validator';
-  import { ApiProperty } from '@nestjs/swagger';
-  
-  export class UpdateStatusDTO {
-    @IsString({ message: 'errors.STATUS_STRING' })
-    @IsNotEmpty({ message: 'errors.STATUS_NOT_EMPTY' })
-    @IsIn(['ACTIVE', 'INACTIVE'], { message: 'errors.STATUS_NOT_VALID' })
-    @ApiProperty({
-      type: String,
-      required: true,
-      description: 'ACTIVE, INACTIVE',
-      example: 'ACTIVE',
-    })
-    status: string;
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class UpdateStatusDTO {
+  @IsString({ message: 'errors.STATUS_STRING' })
+  @IsNotEmpty({ message: 'errors.STATUS_NOT_EMPTY' })
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'ACTIVE, INACTIVE',
+    example: 'ACTIVE',
+  })
+  status: string;
 }
-  
