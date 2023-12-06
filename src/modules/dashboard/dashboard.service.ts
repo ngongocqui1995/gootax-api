f61083d617bfc664e97d9676ce5b794c465d9ba9
Qui <qui.ngo@finviet.com.vn>
2023-12-06 19:59:19 +0700

    update

--- src/modules/dashboard/dashboard.service.ts
+++ src/modules/dashboard/dashboard.service.ts
@@ -3,8 +3,8 @@ import { ENUM_STATUS, ENUM_STATUS_BOOK } from 'src/common';
 import { BaseService } from 'src/common/base.service';
 import { Connection } from 'typeorm';
 import { BookCar } from '../book-cars/entities/book-car.entity';
+import { Customer } from '../customers/entities/customer.entity';
 import { Driver } from '../drivers/entities/driver.entity';
-import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class DashboardService {
@@ -40,38 +40,30 @@ export class DashboardService {
           to_date,
         })
         .getRawOne();
-      const total_user = queryRunner.manager
-        .getRepository(User)
-        .createQueryBuilder('user')
-        .select('count(user.id)', 'sum')
+      const total_customer = queryRunner.manager
+        .getRepository(Customer)
+        .createQueryBuilder('customer')
+        .select('count(customer.id)', 'sum')
         .where('status = :status', { status: ENUM_STATUS.ACTIVE })
-        .andWhere('"createdAt" between :from_date and :to_date', {
-          from_date,
-          to_date,
-        })
         .getRawOne();
       const total_driver = queryRunner.manager
         .getRepository(Driver)
         .createQueryBuilder('driver')
         .select('count(driver.id)', 'sum')
         .where('status = :status', { status: ENUM_STATUS.ACTIVE })
-        .andWhere('"createdAt" between :from_date and :to_date', {
-          from_date,
-          to_date,
-        })
         .getRawOne();
       const results = (
         await Promise.allSettled([
-          total_amount,
-          total_driver,
-          total_user,
           total_order,
+          total_amount,
+          total_customer,
+          total_driver,
         ])
       ).map((it) => (it.status === 'fulfilled' ? +it.value.sum : 0));
 
       data['total_order'] = results[0];
       data['total_amount'] = results[1];
-      data['total_user'] = results[2];
+      data['total_customer'] = results[2];
       data['total_driver'] = results[3];
 
       await queryRunner.commitTransaction();
@@ -96,15 +88,40 @@ export class DashboardService {
     await queryRunner.connect();
     await queryRunner.startTransaction();
     try {
-      data = await queryRunner.manager
+      const total_amount = queryRunner.manager
         .getRepository(BookCar)
-        .createQueryBuilder()
+        .createQueryBuilder('book')
+        .select('sum(book.amount)', 'sum')
+        .addSelect(`"createdAt", 'Doanh số' as title`)
         .where('status = :status', { status: ENUM_STATUS_BOOK.COMPLETED })
         .andWhere('"createdAt" between :from_date and :to_date', {
           from_date,
           to_date,
         })
-        .getMany();
+        .groupBy('"createdAt"')
+        .orderBy({ '"createdAt"': 'ASC' })
+        .getRawMany();
+
+      const total_order = queryRunner.manager
+        .getRepository(BookCar)
+        .createQueryBuilder('book')
+        .select('count(book.id)', 'sum')
+        .addSelect(`"createdAt", 'Đơn hàng' as title`)
+        .where('status = :status', { status: ENUM_STATUS_BOOK.COMPLETED })
+        .andWhere('"createdAt" between :from_date and :to_date', {
+          from_date,
+          to_date,
+        })
+        .groupBy('"createdAt"')
+        .orderBy({ '"createdAt"': 'ASC' })
+        .getRawMany();
+
+      const results = (
+        await Promise.allSettled([total_amount, total_order])
+      ).map((item) => (item.status === 'fulfilled' ? item.value : []));
+
+      data.push(...results[0]);
+      data.push(...results[1]);
 
       await queryRunner.commitTransaction();
     } catch (err) {
@@ -122,7 +139,7 @@ export class DashboardService {
   }
 
   async findOrderStatus(from_date: string, to_date: string) {
-    let data = {};
+    let data = [];
     const queryRunner = this.connection.createQueryRunner();
 
     await queryRunner.connect();
@@ -153,8 +170,10 @@ export class DashboardService {
       const results = (
         await Promise.allSettled([total_order_success, total_order_cancel])
       ).map((it) => (it.status === 'fulfilled' ? +it.value.sum : 0));
-      data['total_order_success'] = results[0];
-      data['total_order_cancel'] = results[1];
+
+      const sum = results[0] + results[1];
+      data.push({ type: 'Thành công', value: (results[0] / sum) * 100 });
+      data.push({ type: 'Huỷ', value: (results[1] / sum) * 100 });
 
       await queryRunner.commitTransaction();
     } catch (err) {
